ff22f9b6b70e0f517e1ff5f0dfccec8eac88cc12
Sridevi2108 <82277962+Sridevi2108@users.noreply.github.com>
2024-11-23 23:14:34 +0530

    Add files via upload

---
+++ Striver/largest_elementinarray.py
@@ -0,0 +1,10 @@
+arr=[2,6,3,9,6]
+largest=arr[0]
+for i in range(len(arr)):
+    if arr[i]>=largest:
+        largest=arr[i]
+print(largest)
+
+
+
+
---
+++ Striver/rough.py
@@ -0,0 +1,14 @@
+# n=3
+# var=0
+# for i in range(1,n):
+#     count = 0
+#     for j in range(1,n):
+#         if i % j == 0:
+#             count = count + 1
+#     if count == 2:
+#         var=var+1
+# print(var)
+import math
+n=27
+power=math.sqrt(n)
+print(power)
